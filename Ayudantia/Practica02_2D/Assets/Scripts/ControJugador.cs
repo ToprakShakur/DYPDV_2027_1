a6a663096a4c237e30f1d75038ac38c2d1b3be26
Toprak Memik Hernandez <toprakshakur@ciencias.unam.mx>
2026-09-19 04:32:56 -0600

    Practica02_2D Enviada

---
+++ Ayudantia/Practica02_2D/Assets/Scripts/ControJugador.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlJugador : MonoBehaviour
+{
+    public float velocidadActual = 0f;
+    public float velocidadMax = 5f;
+    public float aceleracion = 10f;
+
+    public float velocidadVertical = 0f;
+    public float gravedad = -20f;
+
+    public float tiempoMaxSalto = 0.2f;
+    private float tiempoSaltoActual = 0f;
+
+    private float tiempoAnterior;
+
+    private Jugador jugador;
+
+    private void Awake()
+    {
+        jugador = GetComponent<Jugador>();
+    }
+
+    private void Start()
+    {
+        tiempoAnterior = Time.time;
+    }
+
+    private void Update()
+    {
+        // DeltaTime manual
+        float delta = Time.time - tiempoAnterior;
+        tiempoAnterior = Time.time;
+
+        //------------------------------
+        // Movimiento horizontal
+        //------------------------------
+
+        float h = Input.GetAxis("Horizontal");
+
+        velocidadActual += h * aceleracion * delta;
+
+        velocidadActual = Mathf.Clamp(
+        velocidadActual,
+        -velocidadMax,
+        velocidadMax
+        );
+
+        transform.position +=
+        new Vector3(velocidadActual * delta, 0, 0);
+
+        //------------------------------
+        // Salto
+        //------------------------------
+
+        if (Input.GetAxis("Jump") > 0 &&
+        jugador.enSuelo)
+        {
+            velocidadVertical = 10f;
+            jugador.enSuelo = false;
+            tiempoSaltoActual = 0f;
+        }
+
+        if (!jugador.enSuelo &&
+        Input.GetAxis("Jump") > 0)
+        {
+            if (tiempoSaltoActual < tiempoMaxSalto)
+            {
+                velocidadVertical += 20f * delta;
+                tiempoSaltoActual += delta;
+            }
+        }
+
+        if (Input.GetAxis("Jump") == 0)
+        {
+            tiempoSaltoActual = tiempoMaxSalto;
+        }
+
+        if (jugador.enSuelo)
+        {
+            velocidadVertical = 0f;
+        }
+        else
+        {
+            velocidadVertical += gravedad * delta;
+        }
+
+        transform.position +=
+            new Vector3(0, velocidadVertical * delta, 0);
+    }
+}
---
+++ Ayudantia/Practica02_2D/README.md
@@ -0,0 +1,28 @@
+# Proyecto 2D - Práctica 2 (Unity)
+ 
+## Descripcion
+Este proyecto corresponde a la **Práctica 2 de Ayudantía en Unity 2D**, donde se 
+introducen conceptos Fisica Manual de Moviemiento y Salto
+ 
+## Requisitos 
+- Unity Hub instalado.
+- Repositorio del curso configurado.
+- Spritesheet proporcionado o descargado.
+ 
+## Pasos para ejecutar 
+1. Clonar el repositorio:
+```bash
+git clone <https://github.com/ToprakShakur/Programacion_Videojuegos.git>```
+2. Abrir Unity Hub y seleccionar el proyecto de la carpeta
+	Programacion_Videojuegos/Ayudantia/Practica02_2D
+3. Ejecutar la escena desde el editor 
+	Demostracion
+	- El personaje esta creado como Prefab con:
+	- Sprite Renderer
+	- BoxCollider2D
+	- Rigidbody2D
+
+	- Se detectan inputs básicos:
+		- Movimiento laterales (flechas).
+		- Salto (barra espaciadora) = Salto detectado
+
